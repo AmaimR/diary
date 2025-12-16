98a6d69d583b177e14cada4f644c78a167207e41
Dein Name <deine.email@beispiel.com>
2025-12-16 15:42:55 +0100

    Provider: Theme

---
+++ lib/providers/theme_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider with ChangeNotifier {
+  ThemeData get currentTheme {
+    return ThemeData(
+      brightness: Brightness.light,
+      primarySwatch: Colors.blue,
+      scaffoldBackgroundColor: Colors.white,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      ),
+      cardColor: Colors.white,
+      textTheme: const TextTheme(
+        bodyLarge: TextStyle(color: Colors.black),
+        bodyMedium: TextStyle(color: Colors.black),
+        titleLarge: TextStyle(color: Colors.black),
+      ),
+    );
+  }
+}
